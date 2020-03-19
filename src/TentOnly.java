259812140f67d559b260fe3e2714f253c93b5ac5
austinackerman <ackermau@mail.gvsu.edu>
2020-03-19 14:43:37 -0400

    work

--- src/TentOnly.java
+++ src/TentOnly.java
@@ -1,4 +1,6 @@
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -50,29 +52,67 @@ public class TentOnly extends CampSite {
       int inMonth = Integer.parseInt(strCheckInDateMonth);
       int inYear = Integer.parseInt(strCheckInDateYear);
 
-      int tempMonthDay;
-      if (inMonth == 1 || inMonth == 3 || inMonth == 5 || inMonth == 7 || inMonth == 8 ||
-          inMonth == 10 || inMonth == 12) {
-        tempMonthDay = 31;
+
+      int[] outMonths = {1,2,3,4,5,6,7,8,9,10,11,12};
+      int tempOutMonths = 0;
+      int tempOutDays;
+      for (int month : outMonths) {
+        tempOutMonths = month;
       }
-      if (inMonth == 4 || inMonth == 6 || inMonth == 9 || inMonth == 11) {
-        tempMonthDay = 30;
-      }
-      if (inMonth == 2 && inYear % 4 == 0) {
-        if (inYear % 100 == 0) {
-          if (inYear % 400 == 0) {
-            tempMonthDay = 29;
+      switch(tempOutMonths) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+          tempOutDays = 31;
+          break;
+        case 4: case 6: case 9: case 11:
+          tempOutDays = 30;
+          break;
+        case 2:
+          if (outYear % 4 == 0) {
+            if (outYear % 100 == 0) {
+              if (outYear % 400 == 0) {
+                tempOutDays = 29;
+              }
+              else tempOutDays = 28;
+            }
+            else tempOutDays = 29;
           }
-          else tempMonthDay = 28;
-        }
-        else tempMonthDay = 29;
+          else tempOutDays = 28;
+          break;
+        default:
+          throw new IllegalArgumentException("Invalid Month");
       }
-      else tempMonthDay = 28;
-
-
+      
+      int[] inMonths = {1,2,3,4,5,6,7,8,9,10,11,12};
+      int tempInMonths = 0;
+      int tempInDays;
+      for (int month : inMonths) {
+        tempInMonths = month;
+      }
+      switch(tempInMonths) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+          tempInDays = 31;
+          break;
+        case 4: case 6: case 9: case 11:
+          tempInDays = 30;
+          break;
+        case 2:
+          if (outYear % 4 == 0) {
+            if (outYear % 100 == 0) {
+              if (outYear % 400 == 0) {
+                tempInDays = 29;
+              }
+              else tempInDays = 28;
+            }
+            else tempInDays = 29;
+          }
+          else tempInDays = 28;
+          break;
+        default:
+          throw new IllegalArgumentException("Invalid Month");
+      }
 
       numberOfDays += outDay;
-      numberOfDays += (tempMonthDay - inDay);
+      numberOfDays += (tempOutDays - tempInDays);
 
 
       double cost = 10;
