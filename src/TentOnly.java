0838ea17794e447bf81c7a6db94a256f2265e4fb
austinackerman <ackermau@mail.gvsu.edu>
2020-03-19 18:32:41 -0400

    something is wrong with the formatting and in some areas of getCost could probably be done in an easier way should be ok otherwise

--- src/TentOnly.java
+++ src/TentOnly.java
@@ -32,7 +32,7 @@ public class TentOnly extends CampSite {
 
     @Override
     public double getCost(GregorianCalendar checkOut) {
-      SimpleDateFormat formatter = new SimpleDateFormat("dd MM yyyy");
+      SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
       String strCheckOutDate = formatter.format(checkOut);
       String strCheckInDate = formatter.format(checkIn);
 
@@ -52,71 +52,158 @@ public class TentOnly extends CampSite {
       int inMonth = Integer.parseInt(strCheckInDateMonth);
       int inYear = Integer.parseInt(strCheckInDateYear);
 
+      int numberOfDays = 0;
 
-      int[] outMonths = {1,2,3,4,5,6,7,8,9,10,11,12};
-      int tempOutMonths = 0;
-      int tempOutDays;
-      for (int month : outMonths) {
-        tempOutMonths = month;
-      }
-      switch(tempOutMonths) {
-        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-          tempOutDays = 31;
-          break;
-        case 4: case 6: case 9: case 11:
-          tempOutDays = 30;
-          break;
-        case 2:
-          if (outYear % 4 == 0) {
-            if (outYear % 100 == 0) {
-              if (outYear % 400 == 0) {
-                tempOutDays = 29;
+      boolean all31DayMonths = inMonth == 1 || inMonth == 3 || inMonth == 5 || inMonth == 7 || inMonth == 8 ||
+          inMonth == 10 || inMonth == 12;
+      boolean all30DayMonths = inMonth == 4 || inMonth == 6 || inMonth == 9 || inMonth == 11;
+      boolean monthsApart = inMonth + 2 == outMonth || inMonth + 3 == outMonth || inMonth + 4 == outMonth ||
+          inMonth + 5 == outMonth || inMonth + 6 == outMonth || inMonth + 7 == outMonth || inMonth + 8 == outMonth ||
+          inMonth + 9 == outMonth || inMonth + 10 == outMonth || inMonth + 11 == outMonth;
+
+      // when years equal
+      if (outYear == inYear) {
+        // same month
+        if (inMonth == outMonth) {
+          numberOfDays = outDay - inDay;
+        }
+
+        // 1 month apart
+        else if (inMonth + 1 == outMonth) {
+          // Days
+          numberOfDays += outDay;
+          if (all31DayMonths) {
+            numberOfDays = (31 - inDay);
+          } else if (all30DayMonths) {
+            numberOfDays = (30 - inDay);
+          } else if (inMonth == 2) {
+            if (outYear % 4 == 0) {
+              if (outYear % 100 == 0) {
+                if (outYear % 400 == 0) {
+                  numberOfDays = (29 - inDay);
+                }
+                else numberOfDays = (28 - inDay);
               }
-              else tempOutDays = 28;
+              else numberOfDays = (29 - inDay);
             }
-            else tempOutDays = 29;
+            else numberOfDays = (28 - inDay);
           }
-          else tempOutDays = 28;
-          break;
-        default:
-          throw new IllegalArgumentException("Invalid Month");
+        }
+
+        // 2 or more months apart
+        else if (monthsApart) {
+          // Months
+          for (int i = (inMonth + 1); i < outMonth; i++) {
+            if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) {
+              numberOfDays += 31;
+            } else if (i == 4 || i == 6 || i == 9 || i == 11) {
+              numberOfDays += 30;
+            } else if (i == 2) {
+              if (outYear % 4 == 0) {
+                if (outYear % 100 == 0) {
+                  if (outYear % 400 == 0) {
+                    numberOfDays += 29;
+                  } else numberOfDays += 28;
+                } else numberOfDays += 29;
+              } else numberOfDays += 28;
+            }
+          }
+          // Days
+          numberOfDays += outDay;
+          if (all31DayMonths) {
+            numberOfDays = (31 - inDay);
+          } else if (all30DayMonths) {
+            numberOfDays = (30 - inDay);
+          } else if (inMonth == 2) {
+            if (outYear % 4 == 0) {
+              if (outYear % 100 == 0) {
+                if (outYear % 400 == 0) {
+                  numberOfDays = (29 - inDay);
+                } else numberOfDays = (28 - inDay);
+              } else numberOfDays = (29 - inDay);
+            } else numberOfDays = (28 - inDay);
+          }
+        }
       }
-      
-      int[] inMonths = {1,2,3,4,5,6,7,8,9,10,11,12};
-      int tempInMonths = 0;
-      int tempInDays;
-      for (int month : inMonths) {
-        tempInMonths = month;
-      }
-      switch(tempInMonths) {
-        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-          tempInDays = 31;
-          break;
-        case 4: case 6: case 9: case 11:
-          tempInDays = 30;
-          break;
-        case 2:
-          if (outYear % 4 == 0) {
-            if (outYear % 100 == 0) {
-              if (outYear % 400 == 0) {
-                tempInDays = 29;
+
+
+      // when years are not equal
+      else {
+
+        // years
+        numberOfDays += ((outYear - inYear) * 365);
+
+        // same month
+        if (inMonth == outMonth) {
+          numberOfDays = outDay - inDay;
+        }
+
+        // 1 month apart
+        else if (inMonth + 1 == outMonth) {
+          // Days
+          numberOfDays += outDay;
+          if (all31DayMonths) {
+            numberOfDays = (31 - inDay);
+          } else if (all30DayMonths) {
+            numberOfDays = (30 - inDay);
+          } else if (inMonth == 2) {
+            if (outYear % 4 == 0) {
+              if (outYear % 100 == 0) {
+                if (outYear % 400 == 0) {
+                  numberOfDays = (29 - inDay);
+                }
+                else numberOfDays = (28 - inDay);
               }
-              else tempInDays = 28;
+              else numberOfDays = (29 - inDay);
             }
-            else tempInDays = 29;
+            else numberOfDays = (28 - inDay);
           }
-          else tempInDays = 28;
-          break;
-        default:
-          throw new IllegalArgumentException("Invalid Month");
+        }
+
+        // 2 or more months apart
+        else if (monthsApart) {
+          // Months
+          for (int i = (inMonth + 1); i < outMonth; i++) {
+            if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) {
+              numberOfDays += 31;
+            } else if (i == 4 || i == 6 || i == 9 || i == 11) {
+              numberOfDays += 30;
+            } else if (i == 2) {
+              if (outYear % 4 == 0) {
+                if (outYear % 100 == 0) {
+                  if (outYear % 400 == 0) {
+                    numberOfDays += 29;
+                  } else numberOfDays += 28;
+                } else numberOfDays += 29;
+              } else numberOfDays += 28;
+            }
+          }
+          // Days
+          numberOfDays += outDay;
+          if (all31DayMonths) {
+            numberOfDays = (31 - inDay);
+          } else if (all30DayMonths) {
+            numberOfDays = (30 - inDay);
+          } else if (inMonth == 2) {
+            if (outYear % 4 == 0) {
+              if (outYear % 100 == 0) {
+                if (outYear % 400 == 0) {
+                  numberOfDays = (29 - inDay);
+                } else numberOfDays = (28 - inDay);
+              } else numberOfDays = (29 - inDay);
+            } else numberOfDays = (28 - inDay);
+          }
+        }
       }
 
-      numberOfDays += outDay;
-      numberOfDays += (tempOutDays - tempInDays);
+      double cost = 0;
+      if (numberOfTenters > 0 && numberOfTenters <= 10) {
+        cost = numberOfDays * 10;
+      } else if (numberOfTenters > 10) {
+        cost = numberOfDays * 20;
+      }
 
-
-      double cost = 10;
-        return cost;
+      return cost;
     }
 
     @Override
